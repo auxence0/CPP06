71ee444ca17a74120beb19c68c8439adf7d30857
auxence0 <sauvagetauxence@gmail.com>
2026-09-15 14:53:27 +0200

    arrete de m

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: asauvage <asauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/14 18:05:34 by asauvage          #+#    #+#             */
-/*   Updated: 2026/09/14 18:22:20 by asauvage         ###   ########.fr       */
+/*   Updated: 2026/09/15 14:52:50 by asauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ int	main(void) {
 	dta.name = "auxence";
 	dta.size = 1.73;
 	uintptr_t	ptr = Serializer::serialize(&dta);
-	std::cout << std::hex << "unitptr_t: address: " << ptr << "\n";
+	std::cout << std::hex << "unitptr_t: address: " << ptr << "\n" << std::dec;
 	std::cout << "dta: address: " << &dta << "\n";
 	Data*	dta_deserialize = Serializer::deserialize(ptr);
-	std::cout << "After Serializer:\n";
+	std::cout << "After Serializer\n";
 	std::cout << "id: " << dta_deserialize->id << "\n";
 	std::cout << "name: " << dta_deserialize->name << "\n";
 	std::cout << "size: " << dta_deserialize->size << "\n";
